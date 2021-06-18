06034398b32ccc64732d054a6640a3fa2dc8e82e
YoriaDigitalCG <yrios@digitalcg.com.co>
2021-06-18 12:24:48 -0500

    .

--- index.py
+++ index.py
@@ -10,6 +10,20 @@ saludar()
 
 
 #tupla
-
 nombes_asignados=("Juan", "Dante", "Randy", "Venus")
 print(nombes_asignados)
+print(nombes_asignados[0])
+print(nombes_asignados[2])
+print(nombes_asignados[1:3])
+print(nombes_asignados[1:2])
+print(nombes_asignados[2:])
+print(nombes_asignados[:2])
+print(nombes_asignados[-1])
+print(nombes_asignados[-2])
+
+#Lista
+colores_asignados=["Negro", "Café", "Blanc", "Gris"]
+print(colores_asignados)
+print(colores_asignados[1])
+colores_asignados[2] = "Rojo"
+print(colores_asignados[2])
