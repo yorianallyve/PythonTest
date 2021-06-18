242978e4850bd64e58b6239f05b70adfba0705f5
YoriaDigitalCG <yrios@digitalcg.com.co>
2021-06-18 12:13:47 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,15 @@
+def saludar():
+    print("Hola")
+
+def __init__():
+    print("Hi")
+
+print("Saludo 1")
+
+saludar()
+
+
+#tupla
+
+nombes_asignados=("Juan", "Dante", "Randy", "Venus")
+print(nombes_asignados)
